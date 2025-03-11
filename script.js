801f29ceeb2d08d9c4f19225fbabdf9600267ce3
Durga-velu <durgadevi134babu@gmail.com>
2025-03-11 22:35:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 function makeid(l) {
   // write your code here
-	let res=[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,
-			Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,1
-			1,2,3,4,5,6,7,8,9]
-	let ans=[]
+	// let res=[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,
+	// 		Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,1
+	// 		1,2,3,4,5,6,7,8,9]
+	// let ans=[]
 
-	for(let i=0;i<l;i++){
+	let result="ABCDEFGHIJKLMNOPQRSTUVWXYZaBCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
 
-		ans.push(res.[Math.floor(Math.random()*res.length])
+let answer=""
+		for(let i=0;i<l;i++){
+
+		// ans.push(res.[Math.floor(Math.random()*res.length])
+
+answer +=	result.charAt(Math.round(Math.random()*result.length))		
 	}
-	return ans
+	return answer
 }
 
 // Do not change the code below.
