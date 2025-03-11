70f8a523b60ce455089028ed96133241bb6c2aad
Durga-velu <durgadevi134babu@gmail.com>
2025-03-11 22:58:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,17 @@
 function makeid(l) {
   // write your code here
-	// let res=[A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,
-	// 		Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,1
-	// 		1,2,3,4,5,6,7,8,9]
-	// let ans=[]
+	 let res=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z",1,2,3,4,5,6,7,8,9]
+      let ans=[]
+  
+// 	let result="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789"
 
-	let result="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789"
-
-let answer=""
+// let answer=""
 		for(let i=0;i<l;i++){
-
-		// ans.push(res.[Math.floor(Math.random()*res.length])
-
-answer +=	result.charAt(Math.round(Math.random()*result.length))		
+ ans.push(res.[Math.floor(Math.random()*res.length])
+//answer +=	result.charAt(Math.round(Math.random()*result.length))		
 	}
-	return answer
+	return ans
 }
-
-
 // Do not change the code below.
 const l = prompt("Enter a number.");
-alert(makeid(l));
+alert(makeid(l).join(""));
